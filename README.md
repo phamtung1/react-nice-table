2760f05533d430d53874d2e126988903cc8d9756
phamtung <pham.tung1@gmail.com>
2020-05-01 14:19:47 +0700

    Build static storybook

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-nice-table
+
+[Demo](https://phamtung1.github.io/react-nice-table/storybook-static)
---
+++ storybook-static/precache-manifest.0f0b9b9cdd750bb2515bc451cd749031.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23e6d090f84da19a30b6db1b7824bb8",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.1bb845245e43eaa7aa34.bundle.js"
+  },
+  {
+    "url": "runtime~main.1bb845245e43eaa7aa34.bundle.js"
+  },
+  {
+    "revision": "0c25311471a73d086a0a",
+    "url": "static/css/main.d6f4ce81.chunk.css"
+  },
+  {
+    "url": "vendors~main.1bb845245e43eaa7aa34.bundle.js"
+  },
+  {
+    "url": "vendors~main.1bb845245e43eaa7aa34.bundle.js.LICENSE.txt"
+  }
+]);
