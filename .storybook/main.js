94b750c4d89dd20d7fbd28940272c88c112aa496
phamtung <pham.tung1@gmail.com>
2020-05-06 09:38:27 +0700

    Update packages

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,25 +3,6 @@ module.exports = {
   addons: ['@storybook/addon-storysource', '@storybook/preset-typescript'],
   webpackFinal: async config => {
     
-    config.module.rules = config.module.rules.map(rule => {
-      if (
-        String(rule.test) === String(/\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/)
-      ) {
-        return {
-          ...rule,
-          test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/,
-        }
-      }
-    
-      return rule
-    })
-    
-    // use svgr for svg files
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack", "url-loader"],
-    })
-
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
       loader: require.resolve('babel-loader'),
