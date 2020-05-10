1b7947f32932044f8e478c16750dc57f2b584eac
phamtung <pham.tung1@gmail.com>
2020-05-10 15:40:48 +0700

    Build storybook

--- storybook-static/service-worker.js
+++ storybook-static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.318952143a1f8590206f96d85e16f803.js"
+  "precache-manifest.e751133d8d5a94f87078752181cf9850.js"
 );
 
 self.addEventListener('message', (event) => {
