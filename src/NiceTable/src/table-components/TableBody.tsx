fc707f63b0691073a253e8c87a909281ff2d4782
phamtung <pham.tung1@gmail.com>
2020-05-10 15:32:11 +0700

    Add full row selectable

--- src/NiceTable/src/table-components/TableBody.tsx
+++ src/NiceTable/src/table-components/TableBody.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react';
 import { ColumnModel } from '../types/DataModel';
-import TableCell from './TableCell';
-import SelectionTableCell from '../table-components/SelectionTableCell';
+import TableRow from './TableRow';
 import {CheckedState} from '../types/Enum';
 import AppConsts from '../types/AppConsts';
 
@@ -11,10 +10,11 @@ type Props = {
   selection?:boolean;
   onSelectionChange?(rowDataId:any, newState: CheckedState):void;
   selectedRowDataIds?:any[];
+  fullRowSelectable?:boolean;
   dataIdField?:string;
 }
 
-const TableBody:FC<Props> = ({data, columns, selection, onSelectionChange, selectedRowDataIds, dataIdField = AppConsts.DefaultDataIdField}) => {
+const TableBody:FC<Props> = ({data, columns, selection, onSelectionChange, selectedRowDataIds, fullRowSelectable, dataIdField = AppConsts.DefaultDataIdField}) => {
 
   const renderBodyData = () => {
     return data.map((item:any, rowIndex:number) => {
@@ -23,24 +23,19 @@ const TableBody:FC<Props> = ({data, columns, selection, onSelectionChange, selec
         onSelectionChange && onSelectionChange(rowDataId, newState);
       };
 
-      const checkState = selectedRowDataIds && selectedRowDataIds.indexOf(item[dataIdField]) > -1 ? CheckedState.Checked : CheckedState.Unchecked;
+      const isSelected = selectedRowDataIds && selectedRowDataIds.indexOf(item[dataIdField]) > -1;
       
-      const selectionCell = selection && <SelectionTableCell checkedState={checkState} rowDataId={item[dataIdField]} onChange={handleSelectionChange}/>;
-      const rowClassName = selection && checkState === CheckedState.Checked ? 'NiceTable-Row-Selected' : '';
-      
-      return (
-        <tr key={rowIndex} className={rowClassName}>
-          {selectionCell}
-          {columns.map((column:ColumnModel, colIndex:number) => {
-            const content = column.render ? column.render(item) : item[column.field!];
-          return (
-            <TableCell key={colIndex} align={column.align}>
-              {content}
-            </TableCell>          
-          );
-          })}
-        </tr>);
-      });
+      return <TableRow 
+              rowIndex={rowIndex} 
+              columns={columns} 
+              rowData={item} 
+              selection={selection} 
+              selected={isSelected} 
+              dataIdField={dataIdField}
+              onSelectionChange={handleSelectionChange}
+              fullRowSelectable={fullRowSelectable}
+               />
+    });
   }
 
   return (
---
+++ src/NiceTable/src/table-components/TableRow.tsx
@@ -0,0 +1,58 @@
+import React, { FC } from 'react';
+import { ColumnModel } from '../types/DataModel';
+import TableCell from './TableCell';
+import SelectionTableCell from '../table-components/SelectionTableCell';
+import {CheckedState} from '../types/Enum';
+import AppConsts from '../types/AppConsts';
+
+type Props = {
+  rowIndex:number;
+  columns: ColumnModel[];
+  rowData: any;
+  onSelectionChange?(rowDataId:any, newState: CheckedState):void;
+  selection?:boolean;
+  selected?:boolean;
+  fullRowSelectable?:boolean;
+  dataIdField?:string;
+}
+
+const TableRow:FC<Props> = ({rowIndex, columns, rowData, onSelectionChange, selection, selected, fullRowSelectable, dataIdField = AppConsts.DefaultDataIdField}) => {
+
+  const handleCheckboxSelectionChange = () => {
+    if(fullRowSelectable){
+      return;
+    }
+
+    const newState = selected ? CheckedState.Unchecked : CheckedState.Checked;
+    onSelectionChange && onSelectionChange(rowData[dataIdField], newState);
+  };
+
+  const handleRowSelectionChange = () => {
+       if(!selection || !fullRowSelectable){
+         return;
+       }
+
+       const newState = selected ? CheckedState.Unchecked : CheckedState.Checked;
+       onSelectionChange && onSelectionChange(rowData[dataIdField], newState);
+ };
+
+  const checkState = selected ? CheckedState.Checked : CheckedState.Unchecked;
+  
+  const selectionCell = selection && <SelectionTableCell checkedState={checkState} rowDataId={rowData[dataIdField]} onChange={handleCheckboxSelectionChange}/>;
+  const rowClassName = selection && checkState === CheckedState.Checked ? 'NiceTable-Row-Selected' : '';
+  
+  return (
+    <tr key={rowIndex} className={rowClassName} onClick={handleRowSelectionChange}>
+      {selectionCell}
+      {columns.map((column:ColumnModel, colIndex:number) => {
+        const content = column.render ? column.render(rowData) : rowData[column.field!];
+      return (
+        <TableCell key={colIndex} align={column.align}>
+          {content}
+        </TableCell>          
+      );
+      })}
+    </tr>);
+}
+
+export default TableRow;
