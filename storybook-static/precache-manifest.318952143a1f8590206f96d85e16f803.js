1d9acc14fd8207e4c27c6b2f710516c3863b7254
phamtung <pham.tung1@gmail.com>
2020-05-10 14:33:10 +0700

    Build storybook-static

---
+++ storybook-static/precache-manifest.318952143a1f8590206f96d85e16f803.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b140352b00be468edde30bc6f2a7ce27",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.9216db54ad13225f299e.bundle.js"
+  },
+  {
+    "url": "runtime~main.9216db54ad13225f299e.bundle.js"
+  },
+  {
+    "revision": "6f8972df2f8b37d1a54c",
+    "url": "static/css/main.02f474af.chunk.css"
+  },
+  {
+    "url": "vendors~main.9216db54ad13225f299e.bundle.js"
+  },
+  {
+    "url": "vendors~main.9216db54ad13225f299e.bundle.js.LICENSE.txt"
+  }
+]);
